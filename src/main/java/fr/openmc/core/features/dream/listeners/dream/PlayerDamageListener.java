6ecb3bad9794ad13ffd5cd9c903e476e4135750d
iambibi_ <89582596+iambibi@users.noreply.github.com>
2026-03-20 22:24:44 +0100

    Ajout des dégats de chute dans la dimension (1 bloc tombé = -1.5 s) (#1210)
    * update v2.5 branch (#1165)
    
    * Fix soul chestplate altar crafting
    
    * Change SoulAxe rarity from COMMON to RARE
    
    * Correctif de la possibilité de se faire de l'argent a l'infini (#1158)
    
    * fix: claim dupli
    
    * fix: Cannot invoke "String.isEmpty()" because "content" is null
    
    * fix: FancyNpcsPlugin is null
    
    * fix: incorrect singularity transfer (#1157)
    
    * Refonte du principe pour aller dans la dimension des rêves (#1156)
    
    * fix: dream sleep (100%)
    
    * rework: sleep to dream
    
    * impl fall damage
    
    * add sound

--- src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
+++ src/main/java/fr/openmc/core/features/dream/listeners/dream/PlayerDamageListener.java
@@ -1,19 +1,27 @@
 package fr.openmc.core.features.dream.listeners.dream;
 
 import fr.openmc.core.features.dream.DreamUtils;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class PlayerDamageListener implements Listener {
-
     @EventHandler
     public void onFall(EntityDamageEvent event) {
         if (event.getCause() != EntityDamageEvent.DamageCause.FALL) return;
         if (!(event.getEntity() instanceof Player player)) return;
 
         if (DreamUtils.isInDream(player)) {
+            double fallDistance = player.getFallDistance();
+            if (fallDistance < 5) return;
+
+            long secondsLost = (long) (fallDistance * 1.5);
+
+            DreamUtils.removeDreamTime(player, secondsLost, true);
+            player.playSound(player.getEyeLocation(), Sound.ENTITY_PLAYER_BIG_FALL, 1f, 1f);
+
             event.setCancelled(true);
         }
     }

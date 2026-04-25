4174d498e4caf96f30a70e29bdcf97c9bf1a9112
Timéo B. <tbavart@gmail.com>
2026-04-25 21:49:57 +0200

    feat: add permission for /contest settrade (#1254)

--- src/main/java/fr/openmc/core/features/events/contents/weeklyevents/contents/contest/commands/ContestCommand.java
+++ src/main/java/fr/openmc/core/features/events/contents/weeklyevents/contents/contest/commands/ContestCommand.java
@@ -97,6 +97,7 @@ public class ContestCommand {
 
     @Subcommand("settrade")
     @Description("Permet de définir un Trade")
+    @CommandPermission("omc.admin.commands.contest.settrade")
     public void setTrade(
             Player player,
             @Named("trade") @SuggestWith(TradeContestAutoComplete.class) String trade,
